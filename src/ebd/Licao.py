5195f3b27afe3c786420916857fea7ec241e3fc6
Wanderson <wandersonguedesfernandes15@gmail.com>
2024-01-16 09:45:37 -0400

    classe Licao.py implementada

--- src/ebd/Licao.py
+++ src/ebd/Licao.py
@@ -1,8 +1,10 @@
 from datetime import date
 from src.ebd.Classe import Classe
+from src.ebd.Database import Database
 
 
 class Licao:
+    _id: int or None
     _nome: str
     _data: date
     _visitante: int
@@ -10,11 +12,25 @@ class Licao:
     _classe: Classe
 
     def __init__(self, nome, data, visitante, observacao, classe):
+        self._id = None
         self._nome = nome
         self._data = data
         self._visitante = visitante
         self._observacao = observacao
         self._classe = classe
+
+    @property
+    def id(self):
+        if self._id is None:
+            db_instance = Database()
+
+            cursor = db_instance.execute_query(
+                "SELECT (id_licao) from licao where nome=?",
+                (self.nome, )
+            )
+            self._id = cursor.fetchone()[0]
+
+        return self._id
 
     @property
     def nome(self):
@@ -51,3 +67,23 @@ class Licao:
     def set_classe(self, classe):
         self._classe = classe
 
+    def gravar(self, id_pessoa):
+        if self._id is None:
+            db_instance = Database()
+
+            db_instance.execute_query(
+                "INSERT INTO licao (id_pessoa, id_classe, nome, data, visitante, observacao) "
+                "VALUES (?, ?, ?, ?, ?, ?)",
+                (id_pessoa, self.classe.id, self.nome, self.data, self.visitante, self.observacao)
+            )
+            db_instance.commit()
+
+            return self.id
+        else:
+            return None
+
+    @property
+    def licoes(self):
+        db_instance = Database()
+
+        return db_instance.get_table('licao')
