52b8d5cfadf89bcba122e7f9fefe5212a66fad57
Wanderson <wandersonguedesfernandes15@gmail.com>
2024-01-16 09:54:13 -0400

    classe Database.py implementada

--- src/ebd/Database.py
+++ src/ebd/Database.py
@@ -1,12 +1,13 @@
 import sqlite3
-import pandas as od
+import os
 import pandas as pd
 
 
 class Database(object):
     _instance = None
+    _schema_path = '../schema.sqlite'
 
-    def __new__(cls, db_name):
+    def __new__(cls, db_name='sqlite.db'):
         if not hasattr(cls, 'instance') or cls._instance is None:
             cls._instance = super(Database, cls).__new__(cls)
             cls._instance.connection = sqlite3.connect(db_name)
@@ -14,12 +15,30 @@ class Database(object):
 
         return cls._instance
 
+    def __init__(self):
+        self.initializa_schema()
+
+    def initializa_schema(self):
+        schema_path = self._schema_path
+
+        print('Caminho atual - ', os.path.abspath('.'))
+        if not os.path.exists(schema_path):
+            schema_path = './src/schema.sqlite'
+
+        with open(schema_path, 'r') as sqlite_file:
+            sqlite_script = sqlite_file.read()
+
+        self._instance.cursor.executescript(sqlite_script)
+
     def execute_query(self, query, params=None):
         if params is not None:
             self.cursor.execute(query, params)
         else:
             self.cursor.execute(query)
         return self.cursor
+
+    def get_table(self, table_name):
+        return pd.read_sql(f'SELECT * FROM {table_name}', self.connection)
 
     def commit(self):
         self.connection.commit()
@@ -30,13 +49,13 @@ class Database(object):
 
 
 if __name__ == '__main__':
-    db_instance = Database("sqlite.db")
+    db_instance = Database()
 
     # Execute uma consulta de exemplo
     db_instance.execute_query("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)")
     db_instance.execute_query("INSERT INTO users (name) VALUES (?)", ("Forever",))
     df_users = pd.read_sql(
-        "SELECT * FROM users",
+        "SELECT * FROM classe",
         db_instance.connection
     )
     print(df_users)
